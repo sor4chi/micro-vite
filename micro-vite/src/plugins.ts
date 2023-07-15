ca0824f3fa342b501c72ec6535d7e8c93792f1b5
sor4chi <sorakawa0218@gmail.com>
2023-07-15 21:07:05 +0900

    feat(micro-vite): impl reload injection plugin

--- micro-vite/src/plugins.ts
+++ micro-vite/src/plugins.ts
@@ -1,8 +1,10 @@
+import { reload } from "./reloadPlugin";
 import { resolve } from "./resolvePlugin";
 import esbuild from "rollup-plugin-esbuild";
 
 export const getPlugins = () => [
   resolve(),
+  reload(),
   esbuild({
     target: "esnext",
     minify: false,
---
+++ micro-vite/src/reloadPlugin.ts
@@ -0,0 +1,34 @@
+import type { Plugin } from "rollup";
+import { parse } from "node-html-parser";
+
+const virtualScriptId = "/@micro-vite:reload/script.js";
+const virtualScript = `
+  console.log("bar");
+`;
+
+export const reload = (): Plugin => {
+  return {
+    name: "micro-vite:reload",
+    async resolveId(id) {
+      if (id === virtualScriptId) return virtualScriptId;
+      return null;
+    },
+    async load(id) {
+      if (id === virtualScriptId) return virtualScript;
+      return null;
+    },
+    async transform(code, id) {
+      if (!id.endsWith(".html")) return null;
+
+      const doc = parse(code);
+      doc
+        .querySelector("head")
+        ?.insertAdjacentHTML(
+          "beforeend",
+          `<script type="module" src="${virtualScriptId}">`
+        );
+
+      return doc.toString();
+    },
+  };
+};
