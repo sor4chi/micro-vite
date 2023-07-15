2f701a59d93e4566a616228d2e0b3b4a7ccb26e9
sor4chi <sorakawa0218@gmail.com>
2023-07-15 21:19:43 +0900

    feat(micro-vite): impl reload window system from server

--- micro-vite/src/dev.ts
+++ micro-vite/src/dev.ts
@@ -4,10 +4,12 @@ import sirv from "sirv";
 import { transformMiddleware } from "./transformMiddleware";
 import { getPlugins } from "./plugins";
 import { createPluginContainer } from "./pluginContainer";
+import { setupReloadServer } from "./reloadPlugin";
 
 export const startDev = () => {
   const server = connect();
   server.listen(3000, "localhost");
+  const ws = setupReloadServer();
 
   const plugins = getPlugins();
   const pluginContainer = createPluginContainer(plugins);
@@ -27,4 +29,9 @@ export const startDev = () => {
   server.use(historyApiFallback() as any); // ファイルが存在しなかったときにindex.htmlを返すようにするミドルウェア
 
   console.log("dev server running at http://localhost:3000");
+
+  setTimeout(() => {
+    console.log("reload!")
+    ws.send({ type: "reload" });
+  }, 1000 * 5);
 };
